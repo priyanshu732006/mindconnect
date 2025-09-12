62600020529a766cd137a1e28386244e634bab21
Pradyumn Singh <priyanshshiva23@gmail.com>
2025-09-12 17:47:47 +0000

    https://console.firebase.google.com/project/studio-6588365639-fa5e2/data

--- src/lib/firebase/client-app.ts
+++ src/lib/firebase/client-app.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase/client-app.ts
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -10,7 +11,6 @@ const firebaseConfig = {
   "apiKey": "AIzaSyDjTXaJcQU7uak1yh818P-biRjc6-0SDl4",
   "authDomain": "studio-6588365639-fa5e2.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "721521930140",
   "databaseURL": "https://studio-6588365639-fa5e2-default-rtdb.firebaseio.com/"
 };
 
